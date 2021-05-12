89dc38a97239701fcfb4b19683c3ad3ed4023afa
sanjay kranthi <sanjaykranthi@iconassociatess.com>
2021-05-12 20:34:18 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ContComponent } from './cont/cont.component';
+import { FootComponent } from './foot/foot.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    ContComponent,
+    FootComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
